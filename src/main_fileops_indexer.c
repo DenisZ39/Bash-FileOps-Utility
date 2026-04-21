f450f50af7a141b96672463aed1a70c10272f13e
DenisZ39 <zamosteanudenis567@gmail.com>
2026-04-21 21:18:52 +0300

    Am terminat indexer.c

--- src/main_fileops_indexer.c
+++ src/main_fileops_indexer.c
@@ -1,4 +1,4 @@
-#include "../include/db_format.h"
+#include "db_format.h" // functia build din fileops.sh stie sa includa headerul
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -7,10 +7,30 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 #include <errno.h>
-
 #include <stdint.h>
 #include <time.h>
 
+uint32_t checksum_compute(const char* path){
+    int fd = open(path, O_RDONLY);
+    if(fd == -1){
+        return 0;
+    }
+    uint32_t suma = 0;
+    uint32_t buffer; // citim cate 4 octeti
+    int bytes_read;
+    while((bytes_read = read(fd, &buffer, sizeof(uint32_t))) > 0){
+        if(bytes_read < sizeof(uint32_t)){ // daca ultima citire e incompleta lipim
+            uint32_t last_part = 0;
+            memcpy(&last_part, &buffer, bytes_read);
+            suma ^= last_part;
+        }
+        else{
+            suma ^= buffer;
+        }
+    }
+    close(fd);
+    return suma;
+}
 
 int set_lock(int fd, int type, int offset, int len){
     struct flock lock;
@@ -25,43 +45,50 @@ void update(int fd, file_record *file){
     file_record existing_record;
     db_header header;
     int found_pos = -1;
-    set_lock(fd, F_WRLCK, 0, 0);
-    lseek(fd, sizeof(db_header), SEEK_SET);
+    lseek(fd, sizeof(db_header), SEEK_SET); // sarim mereu peste header cand citim file records
     while((read(fd, &existing_record, sizeof(file_record))) == sizeof(file_record)){
-        if(strcmp(existing_record.cale, file->cale) == 0){
-            found_pos = lseek(fd, 0, SEEK_CUR) - sizeof(file_record);
+        if(existing_record.device == file->device && existing_record.inode == file->inode){ // cautam sa vedem daca nu cumva avem deja recordul file in baza de date
+            found_pos = lseek(fd, 0, SEEK_CUR) - sizeof(file_record); // ne uitam dupa inode si device
             break;
         }
     }
     if(found_pos != -1){
+        set_lock(fd, F_WRLCK, found_pos, sizeof(file_record)); // daca gasim actualizam
         lseek(fd, found_pos, SEEK_SET);
         write(fd, file, sizeof(file_record));
+        set_lock(fd, F_UNLCK, found_pos, sizeof(file_record));
     }
     else{
-        lseek(fd, 0, SEEK_END);
-        write(fd, file, sizeof(file_record));
-        lseek(fd, 0, SEEK_SET);
-        read(fd, &header, sizeof(header));
-        header.record_count++;
-        lseek(fd, 0, SEEK_SET);
-        write(fd, &header, sizeof(db_header));
+        lseek(fd, sizeof(db_header), SEEK_SET);
+        while (read(fd, &existing_record, sizeof(file_record)) == sizeof(file_record)) { 
+            if (strcmp(existing_record.cale, file->cale) == 0) { // altfel verificam daca totusi nu exista alt fisier cu acelasi path, dar inode sau device diferite
+                found_pos = lseek(fd, 0, SEEK_CUR) - sizeof(file_record); // ceea ce inseamna ca se modifica file recordul respectiv
+                break;
+            }
+        }
+        if(found_pos != -1){
+            set_lock(fd, F_WRLCK, found_pos, sizeof(file_record));
+            lseek(fd, found_pos, SEEK_SET);
+            write(fd, file, sizeof(file_record));
+            set_lock(fd, F_UNLCK, found_pos, sizeof(file_record));
+        }
+        else{
+            int pos_final = lseek(fd, 0, SEEK_END); // daca nu, dam append la final
+            set_lock(fd, F_WRLCK, 0, sizeof(db_header)); // dam lock pe header ca sa incrementam nr records
+            set_lock(fd, F_WRLCK, pos_final, sizeof(file_record)); // dam lock pe finalul bazei de date ca sa putem scrie file recordul curent
+            lseek(fd, 0, SEEK_END);
+            write(fd, file, sizeof(file_record));
+            lseek(fd, 0, SEEK_SET);
+            read(fd, &header, sizeof(header));
+            header.record_count++;
+            lseek(fd, 0, SEEK_SET);
+            write(fd, &header, sizeof(db_header));
+            set_lock(fd, F_UNLCK, 0, sizeof(db_header));
+            set_lock(fd, F_UNLCK, pos_final, sizeof(file_record));
+        }
     }
-    set_lock(fd,F_UNLCK,0,0);
 }
 
-uint32_t calculeaza_checksum(const char* cale){
-    int fd_fisier=open(cale, O_RDONLY);
-    if(fd_fisier==-1)
-        return 0;
-    uint32_t checksum=0;
-    uint32_t buffer=0;
-    while(read(fd_fisier,&buffer,sizeof(buffer))>0){
-        checksum^=buffer;
-        buffer=0;
-    }
-    close(fd_fisier);
-    return checksum;
-}
 
 void parcurge_recursiv(const char* cale, int fd){
     DIR *dir = opendir(cale);
@@ -70,6 +97,7 @@ void parcurge_recursiv(const char* cale, int fd){
         return ;
     }
     struct dirent *d;
+    struct flock lock;
     struct stat st;
     char cale_noua[4096];
     while((d = readdir(dir)) != NULL){
@@ -84,10 +112,8 @@ void parcurge_recursiv(const char* cale, int fd){
         file.mtime = st.st_mtime;
         file.inode = st.st_ino;
         file.device = st.st_dev;
-        if(S_ISREG(st.st_mode)) {
-            file.type = 1;
-            file.checksum = calculeaza_checksum(cale_noua);
-        }
+        file.checksum = 0;
+        if(S_ISREG(st.st_mode)) file.type = 1, file.checksum = checksum_compute(cale_noua);
         if(S_ISDIR(st.st_mode)) file.type = 2;
         if(S_ISLNK(st.st_mode)) file.type = 3;
         if(S_ISFIFO(st.st_mode)) file.type = 4;
@@ -123,11 +149,11 @@ int main(int argc, char* argv[]){
         perror("eroare deschidere baza de date");
         return 2;
     }
-    set_lock(fd, F_WRLCK, 0, sizeof(db_header));
+    set_lock(fd, F_WRLCK, 0, sizeof(db_header)); 
     db_header header;
-    if((read(fd, &header, sizeof(header))) < (ssize_t)sizeof(header)){
-        memset(&header, 0, sizeof(db_header));
-        memcpy(header.magic, "IDX1", 4);
+    if((read(fd, &header, sizeof(db_header))) < sizeof(db_header)){ // daca initial nu citim sizeof(db_header) biti inseamna ca nu avem un header, adica nu e initializata
+        memset(&header, 0, sizeof(db_header)); // baza de date si o initializam acum
+        strncpy(header.magic, "IDX1", 4);
         header.format_version = 1;
         header.snapshot_state = STATE_OPEN;
         header.active_writers = 1;
@@ -135,19 +161,19 @@ int main(int argc, char* argv[]){
         header.snapshot_id = time(NULL);
     }
     else{
-        if(header.snapshot_state == STATE_SEALED){
+        if(header.snapshot_state == STATE_SEALED){ // daca e sealed iesim
             printf("snapshot sealed\n");
             set_lock(fd, F_UNLCK, 0, sizeof(db_header));
             close(fd);
             return 0;
         }
-        header.active_writers++;
+        header.active_writers++; // daca nu incrementam active writers
     }
     lseek(fd, 0, SEEK_SET);
     write(fd, &header, sizeof(db_header));
     set_lock(fd, F_UNLCK, 0, sizeof(db_header));
 
-    parcurge_recursiv(director, fd);
+    parcurge_recursiv(director, fd); // parcurgem recursiv
 
     set_lock(fd, F_WRLCK, 0, sizeof(db_header));
     lseek(fd, 0, SEEK_SET);
